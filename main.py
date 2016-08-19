f564014b382f2847b94ce27eb7072ae0fc5eb0e2
Marek Jenda <edyp87@gmail.com>
2016-08-19 19:18:34 +0200

    Getting user data from file user_data.

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import UrbanCardDataRetriever
 import getpass
 
-username = input("Prosze podac adres mailowy: " , )
-password = getpass.getpass("Prosze podac haslo: ")
+def getUserDataFromFile():
+    lines = open('user_data').read().splitlines()
+    return (lines[0], lines[1])
 
+def getUserDataFromConsole():
+    username = input("Prosze podac adres mailowy: " , )
+    password = getpass.getpass("Prosze podac haslo: ")
+    return (username, password)
+
+
+username, password = getUserDataFromFile()
 UrbanCardDataRetriever.UrbanCardDataRetriever(username, password).getCardsData()
